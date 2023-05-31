df4b8e38fbfc5e62f19398235bbd7e13100e9d67
kajti0 <kajetanmikrut11@gmail.com>
2023-05-31 14:19:10 +0200

    Revert "Revert "auth + database""
    This reverts commit 089bf195d1ed740dd50935125ce8a865d2b292c3.

---
+++ projekt_backend/DTOs/LoginDto.cs
@@ -0,0 +1,8 @@
+﻿namespace projekt_backend.DTOs
+{
+    public class LoginDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ projekt_backend/DTOs/NewUserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace projekt_backend.DTOs
+{
+    public class NewUserDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
